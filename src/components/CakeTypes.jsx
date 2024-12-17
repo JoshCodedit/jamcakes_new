45d363b72cf7c2a62726a631a332819a4de6971d
JoshCodedit <joshua.dixoncms@gmail.com>
2024-12-17 14:16:00 +0000

    style: cake style section responsive

--- src/components/CakeTypes.jsx
+++ src/components/CakeTypes.jsx
@@ -6,7 +6,7 @@ export default function CakeTypes() {
             <h2 className="heading-text">Our Cakes</h2>
             <p className="body-text">View our range of cakes below</p>
         </div>
-        <div className="flex justify-center items-center gap-10 mb-10">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-10 mb-10">
         <CakeType
             image="./images/wedding-cake.jpeg"
             cakeType="Wedding Cakes"
@@ -16,9 +16,9 @@ export default function CakeTypes() {
             cakeType="Celebration Cakes"
         />
         <CakeType
-                image="./images/cupcakes.jpeg"
-                cakeType="Cupcakes"
-            />
+            image="./images/cupcakes.jpeg"
+            cakeType="Cupcakes"
+        />
         </div>
         </>
     )
